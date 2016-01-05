87f40789324940447ebdbd8f9f107f78e7aad179
¨Minyeop <myjang0507@gmail.com>
2016-01-05 23:52:13 +0900

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Tue Dec 22 02:25:57 KST 2015"
+#define LINUX_COMPILE_BY "fe"
+#define LINUX_COMPILE_HOST "fe"
+#define LINUX_COMPILER "gcc version 5.2.1 20151201 (UBERTC-5.2.x) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.10.9Alphabet_kernel_ver1.0-g78c1619-dirty"
